8cad516e62396f30a317c8b5d2d38c4a52babc2f
月岡祐太 <yuta_tsukioka@tsukiokayuutanoMacBook-Pro.local>
2017-05-15 16:57:20 +0900

    enumクラス名変更2017.5.15

--- src/main/java/controller/NewsListFactory.java
+++ src/main/java/controller/NewsListFactory.java
@@ -9,19 +9,19 @@ import java.util.stream.Stream;
 
 public class NewsListFactory {
     public List<News> getNewsList( String name ){
-        return Stream.of(CastList.values())
+        return Stream.of(Cast.values())
                 .filter( v -> v.name.equals(name))
                 .findFirst().map(v -> v.factory)
                 .orElse(new Nothing())
                 .getNewsList();
     }
 
-    enum CastList {
+    enum Cast {
         新八("菅田将暉", new MasakiSuda());
         final String name;
         final ForNewsIF factory;
 
-        CastList(String name, ForNewsIF factory) {
+        Cast(String name, ForNewsIF factory) {
             this.name = name;
             this.factory = factory;
         }
